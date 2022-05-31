6139a7ba1e0d42bbb2c91bc832ea16695299bc90
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-31 15:05:01 -0700

    added postgresql as db to settings

--- drf_api_permissions_postgres_project/settings.py
+++ drf_api_permissions_postgres_project/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'drf_api_permissions_postgres_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        "HOST": 'db',
+        "PORT": 5432,
     }
 }
 
