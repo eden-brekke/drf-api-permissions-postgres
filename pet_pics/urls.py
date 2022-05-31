6aa84bf4d0d17046e4c1b7367f8881913c622c24
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-31 16:05:40 -0700

    small changes

--- pet_pics/urls.py
+++ pet_pics/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PetPicList, PetPicDetail
 
 urlpatterns = [
-  path("", PetPicList.as_view(), name = 'pet_list'),
-  path('<int:pk>/', PetPicDetail.as_view(), name = 'pet_detail')
+  path("", PetPicList.as_view(), name='pet_list'),
+  path('<int:pk>/', PetPicDetail.as_view(), name='pet_detail')
 ]
