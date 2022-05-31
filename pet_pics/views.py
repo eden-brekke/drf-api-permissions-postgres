c291744be17bb0d85bcccbd30bad7dbc87375716
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-31 14:05:55 -0700

    added list and detail views

--- pet_pics/views.py
+++ pet_pics/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import PetPic
+from .serializers import PetPicSerializer
+
 
 # Create your views here.
+class PetPicList(generics.ListCreateAPIView):
+  queryset = PetPic.objects.all()
+  serializer_class = PetPicSerializer
+  
+  
+class PetPicDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = PetPic.objects.all()
+  serializer_class = PetPicSerializer
+  
