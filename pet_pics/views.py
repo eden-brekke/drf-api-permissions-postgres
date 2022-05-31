db3a74f78b170c23249ac338598ccbab03702b5a
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-31 15:04:41 -0700

    added permissions to detail view

--- pet_pics/views.py
+++ pet_pics/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import PetPic
 from .serializers import PetPicSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 # Create your views here.
@@ -10,6 +11,7 @@ class PetPicList(generics.ListCreateAPIView):
   
   
 class PetPicDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = (IsOwnerOrReadOnly,)
   queryset = PetPic.objects.all()
   serializer_class = PetPicSerializer
   
